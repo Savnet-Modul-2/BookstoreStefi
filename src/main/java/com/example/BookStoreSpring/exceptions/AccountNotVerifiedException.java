a65c06899be0451def0fd6498461abe98db43084
Stefanapo <stefiioana748@yahoo.com>
2025-04-25 19:30:20 +0300

    deleted files

--- src/main/java/com/example/BookStoreSpring/exceptions/AccountNotVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookStoreSpring.exceptions;
-
-public class AccountNotVerifiedException extends RuntimeException {
-    public AccountNotVerifiedException(String message) {
-        super(message);
-    }
-}
